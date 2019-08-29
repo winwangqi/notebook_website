e4b49e5239a3d30bfae2922751205a9305c2dbc1
winwangqi <winwangqi@live.cn>
2019-08-30 01:56:21 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,6 +14,7 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
+    <Link to="/Notebook/功能需求">Go to page 2</Link>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
