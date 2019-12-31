2498e8150c52aa212e0c5fe741bff3319ebdebf0
wangqi <qi_wang@singaporeair.com.sg>
2019-12-31 19:54:35 +0800

    sidebar active node auto centerlize

--- src/components/Tree/index.js
+++ src/components/Tree/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import PropTypes from 'prop-types'
 
 import cns from 'classnames'
@@ -19,23 +19,41 @@ Tree.defaultProps = {
 
 function Tree(props) {
   const { className, treeClassName, nodeClassName, node, nodeCreator, activeID } = props
+  //
+  // const treeNodeElement = useRef(null)
+  // const activeNodeElement = useRef(null)
+  //
+  // useEffect(() => {
+  //   if (treeNodeElement.current && activeNodeElement.current) {
+  //     treeNodeElement.current.scrollTo(
+  //       0,
+  //       activeNodeElement.current.getBoundingClientRect().top -
+  //       (window.screen.availHeight - treeNodeElement.current.getBoundingClientRect().top) / 2
+  //     )
+  //   }
+  // }, [])
 
   return (
-    <div className={cns(treeClassName, className)}>
-      {createNode({ node, nodeCreator, activeID, nodeClassName })}
+    <div
+      ref={treeNodeElement}
+      className={cns(treeClassName, className)}
+    >
+      {createNode({ node, nodeCreator, activeID, nodeClassName, activeNodeElement })}
     </div>
   )
 }
 
-function createNode({ node, nodeCreator, activeID, nodeClassName }) {
+function createNode({ node, nodeCreator, activeID, nodeClassName, activeNodeElement }) {
+  const active = node.id === activeID
+
   return (
-    <div className={cns(nodeClassName, { active: node.id === activeID })}>
+    <div className={cns(nodeClassName, { active })} ref={active ? activeNodeElement : null}>
       {node.label && <div className={styl.label}>{nodeCreator(node)}</div>}
       {node.children && (
         <ul>
           {node.children.map((subNode) => (
             <li key={subNode.id}>
-              {createNode({ node: subNode, nodeCreator, activeID, nodeClassName })}
+              {createNode({ node: subNode, nodeCreator, activeID, nodeClassName, activeNodeElement })}
             </li>
           ))}
         </ul>
