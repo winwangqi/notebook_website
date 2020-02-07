6c87899cdd1d0e7d9074b940d7d3dac7323bbefd
wangqi <qi_wang@singaporeair.com.sg>
2020-02-07 11:53:49 +0800

    birthday version2

---
+++ static/birthday2/README.md
@@ -0,0 +1,32 @@
+Happy Birthday
+
+A Happy Birthday animation design in CSS3, HTML5.
+
+Technology Used: HTML5 CSS3 jQuery
+
+# Setup
+
+## If you have python installed:
+```
+cd Birthday
+```
+
+&& 
+
+```
+python -m SimpleHTTPServer --port  8081
+```
+
+visit http://localhost:8081 in your browser.
+
+## If you have nodejs installed
+```
+npm install
+```
+&&
+
+```
+npm run server-node
+```
+visit http://localhost:8081 in your browser.
+
---
+++ static/birthday2/effect.js
@@ -0,0 +1,130 @@
+$(window).load(function(){
+	$('.loading').fadeOut('fast');
+	$('.container').fadeIn('fast');
+	$('#shine-star').fadeIn('fast')
+});
+$('document').ready(function(){
+  $('#turn_on').click(function(){
+    var $video = $('#open-light-video')
+    var $this = $(this)
+
+    $this.fadeOut('slow')
+
+    $video.show()
+    $video.get(0).play()
+    $video.one('ended', function() {
+      $video.hide()
+      $('#colorful-light').show()
+      $('body').addClass('peach');
+      $('#shine-star').hide()
+      // .delay(1000).promise().done(function(){
+      //   $('#play').fadeIn('slow');
+      // });
+
+      var audio = $('.song')[0];
+      audio.play();
+      $('body').css('backgroud-color','#FFF');
+      $('body').addClass('peach-after');
+
+      $(this).fadeOut('slow').delay(3000).promise().done(function(){
+        $('#bannar_coming').fadeIn('slow');
+      });
+    })
+	});
+	$('#play').click(function(){
+		var audio = $('.song')[0];
+        audio.play();
+		$('body').css('backgroud-color','#FFF');
+		$('body').addClass('peach-after');
+
+    $(this).fadeOut('slow').delay(3000).promise().done(function(){
+      $('#bannar_coming').fadeIn('slow');
+    });
+	});
+
+	$('#bannar_coming').click(function(){
+		$('.bannar').show().addClass('bounceInDown');
+		$(this).fadeOut('slow').delay(3000).promise().done(function(){
+			$('#balloons_flying').fadeIn('slow');
+		});
+	});
+
+	$('#balloons_flying').click(function(){
+		$('#fly-balloons').addClass('animation')
+
+    setTimeout(function() {
+      $('#visible-balloons').addClass('s1')
+    }, 2500)
+
+		$(this).fadeOut('slow').delay(5500).promise().done(function(){
+			$('#cake_fadein').fadeIn('slow');
+		});
+	});
+
+	$('#cake_fadein').click(function(){
+		$('.cake-container').show();
+    $('.pao').addClass('show')
+		$(this).fadeOut('slow').delay(3000).promise().done(function(){
+			$('#light_candle').fadeIn('slow');
+		});
+	});
+
+	$('#light_candle').click(function(){
+		$('.velas').addClass('light');
+		$('#flower').show()
+    $('.pao').addClass('fire')
+    $('#visible-balloons').addClass('s2')
+
+    setTimeout(function() {
+      $('#visible-balloons > div div').fadeIn('slow')
+    }, 3000)
+
+		$(this).fadeOut('slow').delay(5000).promise().done(function(){
+			// $('#wish_message').fadeIn('slow');
+      		$('#story').fadeIn('slow');
+		});
+	});
+
+
+	// $('#wish_message').click(function(){
+	// 	$(this).fadeOut('slow').delay(3000).promise().done(function(){
+	// 		$('#story').fadeIn('slow');
+	// 	});
+	// });
+
+	$('#story').click(function(){
+		$(this).fadeOut('slow');
+
+		$('.bannar').addClass('shrink')
+		$('.cake-container').addClass('shrink')
+
+    $('.message').fadeIn('slow');
+
+		var $plist = $('#msg-list p')
+
+		var len = $plist.length
+
+		function msgLoop (i) {
+      $plist.eq(i).fadeIn('slow').delay(1000).promise().done(function(){
+        $plist.eq(i).fadeOut('slow').delay(600).promise().done(function() {
+          i= i + 1;
+          if (i === len) {
+            $('.bannar').addClass('reset')
+            $('.cake-container').addClass('reset')
+            return
+          }
+          msgLoop(i);
+        })
+      });
+			// body...
+		}
+
+    msgLoop(0);
+
+  });
+});
+
+
+
+
+//alert('hello');
