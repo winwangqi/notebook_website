419cbbb67a87636f5a6a60cb2f045e7b241b6911
wangqi <qi_wang@singaporeair.com.sg>
2020-04-22 10:05:26 +0800

    add vim tutorial

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -10,9 +10,11 @@ import './src/styles/index.scss'
 // image lazy load
 import 'lazysizes'
 
+import './src/plugins/prismjs/themes/prism-tomorrow'
+
 // Auto switch color model by prefers-color-scheme
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-  require('./src/plugins/prismjs/themes/prism-tomorrow')
-} else {
-  require('./src/plugins/prismjs/themes/prism-dracula.css')
-}
+// if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+//   require('./src/plugins/prismjs/themes/prism-tomorrow')
+// } else {
+//   require('./src/plugins/prismjs/themes/prism-dracula.css')
+// }
